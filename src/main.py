af4cb6642137e29a6764be9285a9736319b61a0b
Carlos Davi Dantas <dantasdavi@hotmail.com>
2025-03-31 16:59:20 -0300

    feat: added audio transcriber function

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
 from youtube_audio_downloader import download_audio
+from audio_transcriber import transcribe
 
-downloaded_audio_path = download_audio("https://www.youtube.com/url")
+downloaded_audio_path = download_audio("https://www.youtube.com/shorts/8OJkwyja2MY") #https://www.youtube.com/shorts/8OJkwyja2MY  https://www.youtube.com/watch?v=vFSNUmw32t0
+audio_transcription = transcribe(downloaded_audio_path)
