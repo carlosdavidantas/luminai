20a45f2f1eddedafd9440ee01f39d8c455d3f4fc
Carlos Davi Dantas <dantasdavi@hotmail.com>
2025-06-01 21:12:18 -0300

    feat: add sideBar toggle button

--- src/web-interface/app/src/components/SideBar/SideBar.js
+++ src/web-interface/app/src/components/SideBar/SideBar.js
@@ -1,35 +1,50 @@
 import "./sideBar.css";
 import ChatTitleItem from "../LeftSideTitleChatButton/LeftSideTitleChatButton.js";
+import { TbLayoutSidebarLeftCollapse } from "react-icons/tb";
+import { TbLayoutSidebarLeftExpand } from "react-icons/tb";
+import { useState } from "react";
 
 function SideBar({ setIsNewChat, setMessages, setYouTubeTitle, handleLeftSideChatTitles, youtubeTitle, titles, setTitles, setIsChatBeingDeleted }) {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <div className="LeftSideBackground">
-            <div className="TitleAndNewChatBackground">
-                <h1 className="Title">Luminai</h1>
-                <button className="NewChatButton" onClick={() => {
-                    setIsNewChat(true);
-                    setMessages([]);
-                    setYouTubeTitle("");
-                    handleLeftSideChatTitles(setTitles);
-                }}>
-                    New chat
-                </button>
-            </div>
-
-            <div className="ChatTitleListBackground">
-                <ul className="ChatList">
-                    {titles && titles.map((title, index) => (
-                        <ChatTitleItem
-                            key={index}
-                            title={title}
-                            setYouTubeTitle={setYouTubeTitle}
-                            isSelected={youtubeTitle === title}
-                            onSelect={() => setYouTubeTitle(title)}
-                            setIsNewChat={setIsNewChat}
-                            setIsChatBeingDeleted={setIsChatBeingDeleted}
-                        />
-                    ))}
-                </ul>
+        <div className={`LeftSideBackground${collapsed ? " collapsed" : ""}`}>
+            <button 
+                className="SidebarToggleButton"
+                onClick={() => setCollapsed(!collapsed)}
+            >
+                {collapsed ? (
+                    <TbLayoutSidebarLeftExpand className="SidebarToggleButtonIcon" />
+                ) : (
+                    <TbLayoutSidebarLeftCollapse className="SidebarToggleButtonIcon" />
+                )}
+            </button>
+            <div className={`HideOnCollapse${collapsed ? " hidden" : ""}`}>
+                <div className="TitleAndNewChatBackground">
+                    <h1 className="Title">Luminai</h1>
+                    <button className="NewChatButton" onClick={() => {
+                        setIsNewChat(true);
+                        setMessages([]);
+                        setYouTubeTitle("");
+                        handleLeftSideChatTitles(setTitles);
+                    }}>
+                        New chat
+                    </button>
+                </div>
+                <div className="ChatTitleListBackground">
+                    <ul className="ChatList">
+                        {titles && titles.map((title, index) => (
+                            <ChatTitleItem
+                                key={index}
+                                title={title}
+                                setYouTubeTitle={setYouTubeTitle}
+                                isSelected={youtubeTitle === title}
+                                onSelect={() => setYouTubeTitle(title)}
+                                setIsNewChat={setIsNewChat}
+                                setIsChatBeingDeleted={setIsChatBeingDeleted}
+                            />
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
